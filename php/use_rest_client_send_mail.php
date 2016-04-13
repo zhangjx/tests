c2a9cb118c0565182f5a6db4295021b1a9ba6c1c
zhangjx <zhangjx1990@gmail.com>
2016-04-13 10:24:51 +0800

    Update use_rest_client_send_mail.php
    update

--- php/use_rest_client_send_mail.php
+++ php/use_rest_client_send_mail.php
@@ -4,14 +4,13 @@ require dirname(__FILE__).'/include/rest_client.php';
 
 $token = '83b545f7f6453e6f401dde885282ce8289fd4841';
 $url = '/mails?access_token='.$token;
-$client = new RestClient('http://mail.admasterapi.com', 'json');
+$client = new RestClient('http://mail.api.com', 'json');
 $data = array(
-    'from' => "opennimei@admaster.com.cn",
+    'from' => "opennimei@test.com.cn",
     'from_user_id' => 1,
     'to' => "zhangjx1990@gmail.com",
-    #'to' => "zhangjiaxiang@admaster.com.cn",
     'subject' => "test ni mei",
-    'message' => "test nimei body ",
+    'message' => "test nimei body "
 );
 
 $ret = json_decode($client->post($url, json_encode($data)), true);
